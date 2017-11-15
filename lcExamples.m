8c54080ecc081ee87a7cc51d7a88e907e6928915
David Qiu <david@davidqiu.com>
2017-11-15 11:51:29 -0500

    add eigenvalues (poles) placement example

--- lcExamples.m
+++ lcExamples.m
@@ -4,6 +4,22 @@
 %  - linear classical control (lcc) systems.
 
 
+%% Eigenvalues (Poles) Placement
+%  Find the close-loop control feedback matrix K by defining the placement
+%  of the eigenvalues (poles).
+
+A = [0 1 0;
+     0 0 1;
+     1 5 7];
+B = [1;
+     0;
+     0];
+
+p = [-1+1i, -1-1i, -2];
+
+K = place(A, B, p);
+
+
 %% Laplace Transform
 %  Connecting the state space control systems and classical control systems
 %  by transforming from time domain (t) to frequency domain (s).
