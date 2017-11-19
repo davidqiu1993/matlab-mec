cf68443c9256ea6f8337329817b51d378eb9065d
David Qiu <david@davidqiu.com>
2017-11-19 02:46:14 -0500

    update LTI spaces switching example

--- examples/exlcSwitchSpace.m
+++ examples/exlcSwitchSpace.m
@@ -6,29 +6,37 @@
 %% Laplace Transforms and Inverse Laplace Transforms
 %  Switch from time domain (t) to frequency domain (s).
 
-syms t s
+if 0
+    syms t s
 
-f = cos(pi*t);
+    f = cos(pi*t);
 
-F = laplace(f, t, s);
+    F = laplace(f, t, s);
 
-ff = ilaplace(F, s, t);
+    ff = ilaplace(F, s, t);
+end
 
 
 %% State Space Transformations and Frequency Domain Transfer Functions
 %  Switch between state space control system transformations and frequency 
 %  domain transfer functions.
 
-A = [0 1 0;
-     0 0 1;
-     1 5 7];
-B = [1;
-     0;
-     0];
-C = [0 1 3];
-D = 0;
+if 1
+    A = [0 1 0;
+         0 0 1;
+         1 5 7];
+    B = [1;
+         0;
+         0];
+    C = [0 1 3];
+    D = 0;
 
-[b, a] = ss2tf(A, B, C, D);
+    [b, a] = ss2tf(A, B, C, D);
 
-[AA, BB, CC, DD] = tf2ss(b, a);
+    [AA, BB, CC, DD] = tf2ss(b, a);
+    
+    H = tf(b, a); % get the transfer function
+    
+    step(b, a); % display the step response
+end
 
